1b16bc6f274ab40c1cf3f159ac019f2b7a77ac75
littleneko <litao.little@gmail.com>
2023-06-07 01:12:31 +0800

    [other] remove X-Client-IP header

--- src/server/webserver_libevent.cpp
+++ src/server/webserver_libevent.cpp
@@ -249,7 +249,7 @@ void WebServer::on_request(evhttp_request *req, void *args)
             request.headers.emplace(kv->key, kv->value);
         kv = kv->next.tqe_next;
     }
-    request.headers.emplace("X-Client-IP", client_ip);
+    //request.headers.emplace("X-Client-IP", client_ip);
 
     std::string return_data;
     int retVal = process_request(request, response, return_data);
